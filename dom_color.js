fe73e4b7084da931625bfd2fc32d91b32f1c973f
pooja rani <ranipooja21@navgurukul.org>
2023-10-26 00:38:15 +0530

    done task

---
+++ dom_color.js
@@ -0,0 +1,15 @@
+var but=document.getElementById('red');
+var box=document.getElementById('box');
+but.addEventListener('click' , function onClick(){
+    box.style.backgroundColor='red';
+})
+var but=document.getElementById('yellow');
+var box=document.getElementById('box');
+but.addEventListener('click' , function onClick(){
+    box.style.backgroundColor='yellow';
+})
+var but=document.getElementById('blue');
+var box=document.getElementById('box');
+but.addEventListener('click' , function onClick(){
+    box.style.backgroundColor='blue';
+})
